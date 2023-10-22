5f279f4b8cb851aaa6be2c60c71aa9153c8e6338
Jurandi Junior <jurandijr20@gmail.com>
2023-10-22 00:16:35 -0300

    first commit

---
+++ Challenges/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+namespace Challenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Desafios de lógica em C#");
+        }
+    }
+}
